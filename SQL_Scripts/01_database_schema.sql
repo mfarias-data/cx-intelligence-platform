4734c2335ef17ac20cbc1c33dc1a3bf445513654
Maurício Machado <pkxmau@gmail.com>
2026-06-03 21:47:05 -0300

    Create database schema for CX Analytics DW
    This script creates the database schema for CX Analytics DW, including dimension and fact tables, as well as initial data for dimensions.

---
+++ SQL_Scripts/01_database_schema.sql
@@ -0,0 +1,105 @@
+-- ============================================
+-- CX Intelligence Platform
+-- Database Schema Creation
+-- ============================================
+
+-- Recreate the database for dev env.
+IF EXISTS (SELECT name FROM sys.databases WHERE name = N'CX_Analytics_DW')
+BEGIN
+    ALTER DATABASE CX_Analytics_DW
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+    DROP DATABASE CX_Analytics_DW;
+END
+GO
+
+CREATE DATABASE CX_Analytics_DW;
+GO
+
+USE CX_Analytics_DW;
+GO
+
+-- ============================================
+-- DIMENSIONS
+-- ============================================
+
+CREATE TABLE Dim_Channel (
+    ChannelID INT IDENTITY(1,1) PRIMARY KEY,
+    ChannelName VARCHAR(20) NOT NULL,
+    IsDigital BIT NOT NULL
+);
+
+CREATE TABLE Dim_Category (
+    CategoryID INT IDENTITY(1,1) PRIMARY KEY,
+    CategoryName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE Dim_Agent (
+    AgentID INT PRIMARY KEY,
+    AgentName VARCHAR(100),
+    TeamLeader VARCHAR(100),
+    Department VARCHAR(50)
+);
+
+CREATE TABLE Dim_Calendar (
+    DateKey DATE PRIMARY KEY,
+    CalendarYear INT,
+    CalendarMonth INT,
+    MonthName VARCHAR(20),
+    QuarterNumber INT
+);
+
+-- ============================================
+-- FACT TABLE
+-- ============================================
+
+CREATE TABLE Fact_Interactions (
+    InteractionID UNIQUEIDENTIFIER PRIMARY KEY,
+    InteractionDateTime DATETIME2 NOT NULL,
+
+    CustomerID INT NOT NULL,
+
+    AgentID INT,
+    ChannelID INT NOT NULL,
+    CategoryID INT NOT NULL,
+
+    DurationSeconds INT,
+    ResolutionStatus VARCHAR(20),
+    ReopenedFlag BIT,
+    SatisfactionScore INT,
+
+    CONSTRAINT FK_Fact_Channel
+        FOREIGN KEY (ChannelID)
+        REFERENCES Dim_Channel(ChannelID),
+
+    CONSTRAINT FK_Fact_Category
+        FOREIGN KEY (CategoryID)
+        REFERENCES Dim_Category(CategoryID),
+
+    CONSTRAINT FK_Fact_Agent
+        FOREIGN KEY (AgentID)
+        REFERENCES Dim_Agent(AgentID)
+);
+GO
+
+-- ============================================
+-- INITIAL DIMENSION DATA
+-- ============================================
+
+INSERT INTO Dim_Channel
+(ChannelName, IsDigital)
+VALUES
+('Chat', 1),
+('Voice', 0),
+('Email', 1),
+('Bot', 1);
+
+INSERT INTO Dim_Category
+(CategoryName)
+VALUES
+('Technical Support'),
+('Billing'),
+('Shipping'),
+('Product Info'),
+('Software Update');
+GO
